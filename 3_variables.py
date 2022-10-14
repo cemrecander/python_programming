4a5a924a2b94f3430018cc912f11118ffe26a80a
Cemre Cander <cemrecander@gmail.com>
2022-10-14 13:52:35 +0300

    defined variables and printed

---
+++ 3_variables.py
@@ -0,0 +1,27 @@
+a = 8
+b = None
+c = 1, 75
+d = -6718628565
+print(d)
+
+e = True
+print(e)
+
+print(a, b, c, d, e)
+print(a, b, c, d, e, sep=",")
+
+print(a, b, end=" ")
+print(" same line")
+
+print(a)
+print(b)
+print(c)
+
+a, b, c = 3, 5, 7
+print(a, b, c)
+
+t = "asdf"
+f = 'qwer'
+g = 'a'
+
+print(t, f, g)
