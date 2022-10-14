7121a105da780f153567718a2be9f71831f56e1f
Cemre Cander <cemrecander@gmail.com>
2022-10-14 14:06:03 +0300

    printed variable types

---
+++ 4_variable_types.py
@@ -0,0 +1,23 @@
+a = 9
+b = None
+c = 7.6
+d = "string"
+e = 'c'
+f = 3545468432484687454354873454
+g = False
+
+print("a: ", type(a))
+print("b: ", type(b))
+print("c: ", type(c))
+print("d: ", type(d))
+print("e: ", type(e))
+print("f: ", type(f))
+print("g: ", type(g))
+
+h = [1, 2]
+i = {a: "asdf", d: "dfgh"}
+j = (3, 2, 1)
+
+print("h: ", type(h))
+print("i: ", type(i))
+print("j: ", type(j))
